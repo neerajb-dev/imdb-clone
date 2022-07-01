a0ff4813ba29a85fdab69507a7f49ebbc942cbbf
Neeraj Bhardwaj <neerajb@yahoo.com>
2022-07-01 22:48:43 +0530

    navbar component design change

--- src/routes/navbar/navbar.component.jsx
+++ src/routes/navbar/navbar.component.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 import { MenuDrawer } from '../../components/menuDrawer/menu-drawer.component';
+import BrandLogo from '../../assets/imdb-logo-575.svg';
 import "./navbar.styles.scss";
+import Button from '../../components/button/button.component';
+// import { GiHamburgerMenu } from 'react-icons/gi';
 
 const Navbar = () => {
 
@@ -15,48 +18,21 @@ const Navbar = () => {
 
     return (
         <div className="navbar-container">
-            <nav className="navbar navbar-expand-lg bg-light shadow">
-                <div className="container-fluid">
-                    <Link className="navbar-brand" to="/">Navbar</Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="!#">Link</Link>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <Link className="nav-link dropdown-toggle" to="!#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Dropdown
-                                </Link>
-                                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><Link className="dropdown-item" to="!#">Action</Link></li>
-                                    <li><Link className="dropdown-item" to="!#">Another action</Link></li>
-                                    <li><hr className="dropdown-divider" /></li>
-                                    <li><Link className="dropdown-item" to="!#">Something else here</Link></li>
-                                </ul>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/sign-in" className="nav-link">Sign In</Link>
-                            </li>
-                            <li>
-                                <button className="btn btn-light" onClick={handleMenu}>Menu</button>
-                            </li>
-                        </ul>
-                        <form className="d-flex" role="search">
-                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button className="btn btn-outline-success" type="submit">Search</button>
-                        </form>
-                    </div>
+            <nav className="navbar navbar-expand-lg bg-dark shadow">
+                <div className="container-fluid d-flex justify-content-center align-items-center">
+                    <Link to="/"><img src={BrandLogo} alt="imdb-logo" className="navbar-brand" width={"80px"} /></Link>
+                    <Button
+                        btnType="nav_menu_btn"
+                        btnText="Menu"
+                        // Icon= <GiHamburgerMenu size="24px" className="me-2" />
+                        handleClick={handleMenu}
+                     />
+
                 </div>
             </nav>
 
             {
-                showMenu && <MenuDrawer handleMenu={handleMenu} /> 
+                showMenu && <MenuDrawer handleMenu={handleMenu} />
             }
 
             <Outlet />
